73efb29a040882091a572acd1779db592a1c6b31
MVoldemar <65668419+MVoldemar@users.noreply.github.com>
2021-03-13 22:45:35 +0300

    4

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -25,10 +25,10 @@ class Bender (var status : Status = Status.NORMAL, var question : Question = Que
 
 
     enum class Status(val color: Triple<Int, Int, Int>){
-        NORMAL(Triple(255, 255, 255)),
+        NORMAL(Triple(255, 255, 255)) ,
         WARNING(Triple(255, 120, 0)),
         DANGER(Triple(255, 60, 60)),
-        CRITICAL(Triple(255, 255, 0));
+        CRITICAL(Triple(255, 0, 0)) ;
 
         fun nextStatus():Status{
 
@@ -41,19 +41,19 @@ class Bender (var status : Status = Status.NORMAL, var question : Question = Que
         }
     }
     enum class Question(val question: String, val answers:List<String>) {
-        NAME("Как меня зовут?", listOf("бендер", "bender")) {
+        NAME("Как меня зовут?", listOf("Бендер", "bender")) {
             override fun nextQuestion(): Question = PROFESSION
         },
         PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")){
             override fun nextQuestion(): Question = MATERIAL
         },
-        MATERIAL("Из чего я сделан?", listOf("метал", "дерево", "metal", "iron", "wood")){
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")){
             override fun nextQuestion(): Question = BDAY
         },
         BDAY("Когда меня создали?", listOf("2993")){
             override fun nextQuestion(): Question = SERIAL
         },
-        SERIAL("Мой серийный номер", listOf("2716057")) {
+        SERIAL("Мой серийный номер?", listOf("2716057")) {
             override fun nextQuestion(): Question = IDLE
         },
         IDLE("На этом все, вопросов больше нет", listOf()){
